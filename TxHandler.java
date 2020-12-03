a4e3b516f6c5d90792f84184c55dc2b2089a7f98
mmagdys <muhammad.magdy50@gmail.com>
2020-12-03 13:23:53 +0200

    Transcations handler checking for valadity

--- TxHandler.java
+++ TxHandler.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class TxHandler {
 	
-	    private UTXOPool utxoPool;
+	private UTXOPool utxoPool;
 
     /**
      * Creates a public ledger whose current UTXOPool (collection of unspent transaction outputs) is
@@ -28,37 +28,85 @@ public class TxHandler {
 
         UTXOPool tmpUtxoPool = new UTXOPool(this.utxoPool);
         UTXO tmpUTXO;
+        double outSum = 0;
+        double inSum = 0;
+        Transaction.Output prevTx;
+//        System.out.println("Validation of TX: " + input.prevTxHash);
+
         for (Transaction.Input input : tx.getInputs()){
             tmpUTXO = new UTXO(input.prevTxHash, input.outputIndex);
             if(tmpUtxoPool.contains(tmpUTXO)){
-                System.out.println("yes");
+                prevTx = tmpUtxoPool.getTxOutput(tmpUTXO);
+//                System.out.println("yes", prevTrans.address);
+                if(verifySignature(prevTx.address, tx.getRawTx(), input.signature)){
+                    inSum += prevTx.value;
+                    tmpUtxoPool.removeUTXO(tmpUTXO);
+                }
+                else{
+                    return false;
+                }
+                
             }
             else{
                 System.out.println("no");
+                return false;
             }
-        //     Transaction prevTrans = getTransaction(input.prevTxHash).outputs[outputIndex];
-        //    if( prevTrans ){
-        //        verifySignature(prevTrans.address, prevTrans.getRawDataToSign(), input.signature)
-
-        //    }
-        //    else{
-        //        return false;
-        //    }
-        	System.out.println(Arrays.toString(input.prevTxHash));
-        	System.out.println((input.prevTxHash));
-
-        	
-            
-
         }
-        return true;
-
-        // for (Output output : tx.outputs){
-        //     if(this.utxoPool.contains(output))
-
-        // }
         
+        for (Transaction.Output output : tx.getOutputs()){
+            if(output.value >= 0)
+                outSum += output.value;
+            else
+                return false;
+        }
+
+        if (inSum >= outSum)
+            return true;
+
+        return false;    
     }
+    
+    public UTXOPool isValidTx(Transaction tx, UTXOPool tmpUtxoPool) {
+        // IMPLEMENT THIS
+
+        UTXO tmpUTXO;
+        double outSum = 0;
+        double inSum = 0;
+        Transaction.Output prevTx;
+
+        for (Transaction.Input input : tx.getInputs()){
+            tmpUTXO = new UTXO(input.prevTxHash, input.outputIndex);
+            if(tmpUtxoPool.contains(tmpUTXO)){
+                prevTx = tmpUtxoPool.getTxOutput(tmpUTXO);
+                if(verifySignature(prevTx.address, tx.getRawTx(), input.signature)){
+                    inSum += prevTx.value;
+                    tmpUtxoPool.removeUTXO(tmpUTXO);
+                }
+                else{
+                    return null;
+                }
+                inSum += tmpUtxoPool.getTxOutput(tmpUTXO).value;
+                tmpUtxoPool.removeUTXO(tmpUTXO);
+            }
+            else{
+                System.out.println("no");
+                return null;
+            }
+        }
+        
+        for (Transaction.Output output : tx.getOutputs()){
+            if(output.value >= 0)
+                outSum += output.value;
+            else
+                return null;
+        }
+
+        if (inSum >= outSum)
+            return tmpUtxoPool;
+
+        return null;
+    }
+
 
     /**
      * Handles each epoch by receiving an unordered array of proposed transactions, checking each
@@ -67,19 +115,30 @@ public class TxHandler {
      */
     public Transaction[] handleTxs(Transaction[] possibleTxs) {
         // IMPLEMENT THIS
+        ArrayList<Transaction> validTransactions = new ArrayList<Transaction>();
+        UTXOPool tmpUtxoPool = new UTXOPool(this.utxoPool);
         UTXO tmpUTXO;
+
         for (Transaction tx : possibleTxs){
-            // if (this.isValidTx(tx)){
-            //     System.out.println("valid");
-            // }
-            for (int i = 0; i < tx.getOutputs().size(); i++){
-                System.out.println("Adding TX of hash: " + tx.getHash());
-//                tmpUTXO = new UTXO(tx.getHash(), i);
-            }
-            
-            
+                ArrayList<Transaction.Output> outputs = tx.getOutputs();
+                for (int i = 0; i < outputs.size(); i++){
+                    System.out.println("Adding TX of hash: " + tx.getHash());
+                    tmpUTXO = new UTXO(tx.getHash(), i);
+                    tmpUtxoPool.addUTXO(tmpUTXO, outputs.get(i));   
+                }
         }
-    	return null;
+
+        for (Transaction tx : possibleTxs){
+            UTXOPool newPool = this.isValidTx(tx, tmpUtxoPool);
+            if (newPool == null){
+                System.out.println("NOT valid");
+            }
+            else{
+                tmpUtxoPool = newPool;
+                validTransactions.add(tx);
+            }
+        }
+    	return validTransactions.toArray(new Transaction[0]);
     }
     
     
